4847be79f6334d44b96dac9bff09076b79b0cd17
Sai Lakshmi <154447617+Sai-lakshmi-p@users.noreply.github.com>
2024-09-26 14:52:03 +0530

    Add files via upload

--- sampledemo.py
+++ sampledemo.py
@@ -1,4 +1,3 @@
-import os
 import requests
 from flask import Flask
 
@@ -74,4 +73,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=5001)
